5ad7e34755309898243975c25bcb9ac6813f61f2
Mahipal73 <116993931+Mahipal73@users.noreply.github.com>
2023-04-03 00:24:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main extends TicTacToe{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        for( int n : playerPositions){
+            System.out.println(n);
+        }
+    }
+}
